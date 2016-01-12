0e17c89642e440b68947d7250bd38a91e34d5439
Yaoming9 <geekator.roche@gmail.com>
2016-01-12 15:20:36 +0100

    Refonte menu avec drupal

--- drupal-7.41/sites/all/themes/MonTheme/js/main.js
+++ drupal-7.41/sites/all/themes/MonTheme/js/main.js
@@ -1,6 +1,13 @@
 jQuery(document).ready(function($) {
+	
+	$('li').children('ul').removeClass('menu').addClass('submenu');
+	$('li').removeClass('leaf').removeClass('expanded').removeClass('collapsed');
+	
+	$('.last').css('border','none');
+	
 	$('.submenu').hide();	
-	$('.menu-item').hover(function () {
+		
+	$('.menu').children('li').hover(function () {
 		$(this).css('background-color', '#446CB3');
 		$(this).css('cursor', 'pointer');
 		$(this).children('a').css('color', 'white');
@@ -14,10 +21,20 @@ jQuery(document).ready(function($) {
 	});
 	
 	$('.menu-item-mobile').hover(function () {
+		$('#hamburger-menu').css('-webkit-transition', 'transform .5s');
+		$('#hamburger-menu').css('transition', 'transform .5s');
+		$('#hamburger-menu').css('-webkit-transform', 'rotate(90deg)');
+		$('#hamburger-menu').css('-ms-transform', 'rotate(90deg)');
+		$('#hamburger-menu').css('transform', 'rotate(90deg)');
 		$(this).children('.submenu').stop(true, false).slideDown();
 		},
 
 		function () {
+			$('#hamburger-menu').css('-webkit-transition', 'transform .5s');
+			$('#hamburger-menu').css('transition', 'transform .5s');
+			$('#hamburger-menu').css('-webkit-transform', 'rotate(0deg)');
+			$('#hamburger-menu').css('-ms-transform', 'rotate(0deg)');
+			$('#hamburger-menu').css('transform', 'rotate(0deg)');
 			$(this).children('.submenu').stop(true, false).slideUp();
 	});
 });
--- drupal-7.41/sites/all/themes/MonTheme/templates/page.tpl.php
+++ drupal-7.41/sites/all/themes/MonTheme/templates/page.tpl.php
@@ -1,56 +1,10 @@
   <div id="wrapper">
     <div id="container" class="clearfix">
+	<br><br><br>
       <div id="header">
-
-		<ul id="main_menu">
-			<img id="logo" src="<?php print $logo ?>"/>
-			<li class="menu-item">
-				<a href="#">Accueil</a></li>
-			<li class="menu-item first">
-				<a href="#">Formation</a>
-				<ul class="submenu">
-					<li class="submenu-item"><a href="#">Présentation</a></li>
-					<li class="submenu-item"><a href="#">Emploi du temps</a></li>
-					<li class="submenu-item"><a href="#">Programme</a></li>
-					<li class="submenu-item"><a href="#">Stage</a></li>
-					<li class="submenu-item"><a href="#">Pôle Lardy et UBP</a></li>
-					<li class="submenu-item"><a href="#">Notre groupe de recherche</a></li>
-				</ul>
-			</li>
-			<li class="menu-item">
-				<a href="#">Métiers</a>
-				<ul class="submenu">
-					<li class="submenu-item"><a href="#">Administrateur réseau</a></li>
-					<li class="submenu-item"><a href="#">Analyste / Programmeur</a></li>
-					<li class="submenu-item"><a href="#">Chef de projet</a></li>
-					<li class="submenu-item"><a href="#">Community manager</a></li>
-					<li class="submenu-item"><a href="#">Webdesigner</a></li>
-					<li class="submenu-item"><a href="#">Webmarketer</a></li>
-					<li class="submenu-item"><a href="#">Webmaster</a></li>
-				</ul>
-			</li>
-			<li class="menu-item">
-				<a href="#">Étudiants</a>
-				<ul class="submenu">
-					<li class="submenu-item"><a href="#">Modalités d'admission</a></li>
-					<li class="submenu-item"><a href="#">Suivis de candidature</a></li>
-					<li class="submenu-item"><a href="#">Association étudiante</a></li>
-					<li class="submenu-item"><a href="#">Carte Campus</a></li>
-				</ul>
-			</li>
-			<li class="menu-item">
-				<a href="#">Entreprises</a>
-				<ul class="submenu">
-					<li class="submenu-item"><a href="#">Nos partenaires</a></li>
-					<li class="submenu-item"><a href="#">Taxe d'apprentissage</a></li>
-					<li class="submenu-item"><a href="#">Poster une offre de stage ou d'emploi</a></li>
-				</ul>
-			</li>
-			<li class="menu-item">
-				<a href="#">Contact</a>
-			</li>
-		</ul>		
-		
+		<img id="logo" src="<?php print $logo ?>"/>
+		<?php print render($page['header']); ?>
+				
 		<!-- Menu sur mobile -->
 		<ul id="main_menu_mobile">
 			<img id="logo_mobile" src="<?php print $logo ?>"/>
